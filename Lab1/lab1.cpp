397be46f035d2b8dd00482d9f17ea43b0d0cabbc
yee_thao <yeethao530@hotmail.com>
2016-08-24 17:31:07 +0000

    Lab1 folder ascii art

---
+++ Lab1/lab1.cpp
@@ -0,0 +1,17 @@
+// Yee Thao
+// 8-24-2016
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "      *  " << endl;
+    cout << "    *   *" << endl;
+    cout << "  *   *   *" << endl;
+    cout << "*   *   *   *  " << endl;
+    cout << "      *" << endl;
+    cout << "      *" << endl;
+    cout << "      *" << endl;
+    cout << "      *" << endl;
+}
