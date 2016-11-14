ee9a370e931bda01094415a037ed82376f65ee3f
yee_thao <yeethao530@hotmail.com>
2016-11-14 00:18:58 +0000

    Lab2 submission

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -1,12 +1,15 @@
-/*
-Computer will randomly generate a number between 1 and 10. It will then prompt
-the user to enter a number between 1 and 10. A code will compare the two numbers,
-if the generated number is greater than the user input then it will then display 
-"higher" and ask for the user to input again. If the computer generated number is 
-less than the user input then it would display "lower" and again ask for the user 
-enter another number. When the user gets the number right it will add 10 to the 
-user's score. In the beginning the user starts with zero so if they get one right 
-10 will be added and they will end up with 10 points. It will also be displayed 
-so the user can keep track of their score. Each time they get a guess wrong one 
-will be taken away from the total score of the user.
-*/
+// Yee Thao
+// 8-26-2016
+
+#include <iostream>
+using namespace std;
+
+void mySong(string animal, string object){
+    cout << "Five little " << animal << " jumping on the " << object << " one fell down and broke his crown." << endl;
+    cout << "Momma called the doctor and the doctor said" << endl;
+    cout << "No more " << animal << " jumping on the " << object << "." << endl;
+}
+
+int main(){
+    mySong("monkey","bed");
+}
